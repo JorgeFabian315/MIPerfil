f30c0fa90a08c6c8f81130f84c97a3a0eb58d4f6
Fabian315 <jfabi@FABIAN>
2023-09-28 15:04:51 -0600

    Agregar archivos de proyecto.

---
+++ MIPerfil/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIPerfil.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MiPerfil()
+        {
+            return View();
+        }
+    }
+}
